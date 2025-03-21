646a2bbeb5cbfa9fdda2f40523176c0abcef5b30
JulianBesanz <darkvaan97@gmail.com>
2025-03-20 20:04:16 -0500

    enunciado ejercio 1.5

---
+++ ejercicios/ejercicio_1_5.py
@@ -0,0 +1,14 @@
+'''
+Escriba un programa que realice la comprobación 
+de la división. Recuerde: 
+Divisor * Cociente + Residuo = Dividendo
+Tome como entrada dos números, realice la 
+división entre ellos y entregue al usuario un texto 
+descriptivo con la comprobación de la división.
+Ejercicio 1.6
+Teniendo en cuenta el ejercicio anterior calcule el 
+residuo con el símbolo de módulo % y entregue la 
+comprobación con los valores resultantes de dividir 
+dos números entregados por el usuario del 
+programa.
+'''
