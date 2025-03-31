8b7267756ea9b721a96820cf21a601f9d35c1461
JulianBesanz <darkvaan97@gmail.com>
2025-03-31 16:57:11 -0500

    ejercicios_31marzoOK

--- ejercicios/ejercicio10.py
+++ ejercicios/ejercicio10.py
@@ -2,3 +2,20 @@
 Problema: Crear un diccionario con algunos productos 
 y permitir que el usuario agregue un nuevo producto con su precio.
 '''
+
+
+productos = {
+    "pera": 3.000,
+    "manzana": 1.500,
+    "banano": 500,
+    "limon": 400,
+    "sandia": 7000
+
+}
+
+nombre = input("cual producto desea agregar")
+precio = float(input("ingrese el valor "))
+
+productos[nombre] = precio #agregaria un nuevo producto al diccionario ya creado
+
+print("productos:", productos) #muestra la el diccionario actualizado
--- ejercicios/ejercicio9.py
+++ ejercicios/ejercicio9.py
@@ -2,3 +2,14 @@
 Problema: Crear un diccionario con las calificaciones de 3 estudiantes 
 y permitir que el usuario consulte la calificación de uno de ellos.
 '''
+
+estudiantes = {"juan": 9,
+                "julian": 10, 
+                "armando": 5,
+                "sofia": 3, 
+                "cristal": 2}
+
+nombre = input("ingrese nombre  de estudiante  a consultar")
+
+calificacion = estudiantes.get(nombre, "estudiante no encontrado")
+print("califacion:", calificacion)
